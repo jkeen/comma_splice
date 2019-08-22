c7f423290b347a33d3ffc67d3c0243a625279e2c
Alex Mooney <AlexMooney@fastmail.com>
2019-08-22 14:42:33 -0700

    Simplify the value slicing
    It could be more elegant if right_bounds was positive, but then the
    slices for the Corrector would be uglier.

--- lib/comma_splice/line_corrector.rb
+++ lib/comma_splice/line_corrector.rb
@@ -35,17 +35,8 @@ module CommaSplice
       # the only values that could contain an extra comma are "artist,title,albumtitle,label"
       # therefore our left_bounds = 4, right_bounds = -5
 
-      values_before = if left_bounds > 0
-                        values[0..(left_bounds - 1)]
-                      else
-                        []
-                      end
-
-      values_after =  if right_bounds < -1
-                        values[(right_bounds + 1)..-1]
-                      else
-                        []
-                      end
+      values_before = values[0...left_bounds]
+      values_after = values.slice(right_bounds + 1, -(right_bounds + 1))
       [values_before, corrector.correction, values_after].flatten.join(',')
     end
 
